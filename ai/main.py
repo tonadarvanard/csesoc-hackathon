164c5337c7e07d9dd5a01ebaba3dfa8ff2f51e55
jonbttt <thamrinjonathan@gmail.com>
2024-06-30 05:32:05 +1000

    added testdata and runs

--- ai/main.py
+++ ai/main.py
@@ -1,7 +1,5 @@
-from fastapi import FastAPI
 from ultralytics import YOLO
+import cv2
 
 model = YOLO('yolov8m-pose.pt')
 results = model(source = 0, show = True, conf = 0.3, save = True)
-
-app = FastAPI()
