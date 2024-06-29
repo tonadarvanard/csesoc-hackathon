b64cd19ab6511c705115086e7fa1c205908c1f6d
Daivaaddia <dkfaddia@gmail.com>
2024-06-29 17:07:12 +1000

    Implemented fetching user data and user progress data in dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../lib/auth";
+import { db } from "../lib/db";
 
 const Page = async () => {
   const session = await getServerSession(authOptions);
@@ -8,7 +9,42 @@ const Page = async () => {
     return <div>Please log in!</div>;
   }
 
-  return <div>Welcome to your Dashboard, {session?.user?.name}</div>;
+  const user = await db.user.findUnique({
+    where: { email: `${session.user.email}` },
+  });
+
+  if (!user) {
+    return <div>User not found...</div>;
+  }
+
+  const progressIds = user.progressIds;
+  let progs = await db.progress.findMany({
+    where: { id: { in: progressIds } },
+  });
+  progs = progs.reverse();
+
+  return (
+    <div>
+      <div className="flex flex-col">
+        <div>Name: {user.id}</div>
+        <div>Name: {user.name}</div>
+        <div>Email: {user.email}</div>
+      </div>
+
+      <div className="flex flex-col gap-4">
+        {progs.map((prog, i) => (
+          <div key={i} className="border">
+            <p>{prog.id}</p>
+            <p>{`${prog.timestamp}`}</p>
+            <p>{prog.formMatchPercent}</p>
+            <p>{prog.reps}</p>
+            <p>{prog.sets}</p>
+            <p>{prog.rpe ?? "No rpe"}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Page;
--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -51,5 +51,4 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   ],
-  
 };
