5eaff254493bd4acb577f188c7e1790ba6a77de6
Daivaaddia <dkfaddia@gmail.com>
2024-06-30 00:07:35 +1000

    Recorder now records full videos

--- app/components/record.tsx
+++ app/components/record.tsx
@@ -1,65 +1,96 @@
 "use client";
 
-import { useEffect, useRef } from "react";
-import Link from "next/link";
-
+import { useRef, useState } from "react";
 export default function Record() {
   const videoRef = useRef<HTMLVideoElement>(null);
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [mediaRecorder, setMediaRecorder] = useState<MediaStreamTrack | null>(
+    null
+  );
+  const [Recorder, setRecorder] = useState<MediaRecorder | null>(null);
+  const [isRecording, setIsRecording] = useState(false);
 
-  useEffect(() => {
-    const startVideo = async () => {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({
-          video: true,
-        });
-        if (videoRef.current) {
-          videoRef.current.srcObject = stream;
-          videoRef.current.play();
-          setInterval(captureFrame, 500);
-        }
-      } catch (error) {
-        console.error("Error accessing camera:", error);
+  const startRecording = async () => {
+    setIsRecording(true);
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+    });
+    const recorder = new MediaRecorder(stream);
+
+    if (videoRef.current) {
+      videoRef.current.srcObject = stream;
+      videoRef.current.play();
+    }
+
+    setRecorder(recorder);
+    setMediaRecorder(stream.getVideoTracks()[0]);
+    const recordedChunks: Blob[] = [];
+    recorder.ondataavailable = (e) => {
+      console.log("a");
+      if (e.data.size > 0) {
+        recordedChunks.push(e.data);
       }
     };
-    startVideo();
-  }, []);
 
-  const captureFrame = async () => {
-    if (!canvasRef.current || !videoRef.current) return;
+    recorder.onstop = () => {
+      const blob = new Blob(recordedChunks, { type: "video/webm" });
+      //sendVideo(blob);
+      const url = URL.createObjectURL(blob);
 
-    const canvas = canvasRef.current;
-    const context = canvas.getContext("2d");
-    if (!context) return;
+      // const a = document.createElement("a");
+      // a.style.display = "none";
+      // a.href = url;
+      // a.download = "blob.mp4";
+      // document.body.appendChild(a);
+      // a.click();
 
-    context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
-    const frame = canvas.toDataURL("image/jpeg");
+      setIsRecording(false);
+      if (mediaRecorder) {
+        mediaRecorder.stop();
+      }
+    };
 
+    recorder.start();
+  };
+
+  const stopRecording = () => {
+    Recorder?.stop();
+  };
+
+  const sendVideo = async (videoBlob: Blob) => {
     try {
-      //   const response = await fetch("/api/process_video", {
-      //     method: "POST",
-      //     body: frame,
-      //     headers: {
-      //       "Content-Type": "application/octet-stream",
-      //     },
-      //   });
-      //   const result = await response.json();
-      //   console.log(result);
-      console.log(frame);
+      // const formData = new FormData();
+      // formData.append("video", videoBlob, "recorded-video.webm");
+      // const response = await fetch("/api/upload_video", {
+      //   method: "POST",
+      //   body: formData,
+      // });
+      // if (response.ok) {
+      //   console.log("Video uploaded successfully!");
+      // } else {
+      //   console.error("Failed to upload video:", response.statusText);
+      // }
+      // if (videoRef.current) {
+      //   videoRef.current.src = url;
+      // }
     } catch (error) {
-      console.error("Error processing video:", error);
+      console.error("Error uploading video:", error);
+    }
+  };
+
+  const handleStartStopClick = () => {
+    if (isRecording) {
+      stopRecording();
+    } else {
+      startRecording();
     }
   };
 
   return (
     <div>
       <video ref={videoRef} width="640" height="480"></video>
-      <canvas
-        ref={canvasRef}
-        width="640"
-        height="480"
-        style={{ display: "none" }}
-      ></canvas>
+      <button onClick={handleStartStopClick}>
+        {isRecording ? "Stop Recording" : "Start Recording"}
+      </button>
     </div>
   );
 }
